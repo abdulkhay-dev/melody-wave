9e40a2f08d54eaadfa39e17c060f6bd17af924f9
abdulkhay-dev <dev.abdulkhay@gmail.com>
2025-01-12 08:27:01 +0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    // Здесь вы можете добавить свои кастомные правила
+    'vue/multi-word-component-names': 'off', // Отключение проверки имен компонентов
+  },
+};
