55edf29cb030ee33127d9c783ac74bbd63e7ebc1
abdulkhay-dev <dev.abdulkhay@gmail.com>
2025-01-12 18:16:12 +0500

    CONFIG: prettier and eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,16 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     // Здесь вы можете добавить свои кастомные правила
-    'vue/multi-word-component-names': 'off', // Отключение проверки имен компонентов
+    "vue/multi-word-component-names": "off", // Отключение проверки имен компонентов
   },
-};
+}
